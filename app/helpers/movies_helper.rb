5d7c43f44b41b93afe71aa9078c5e57f98576d05
Jefry Daniel Peña <72535113+jdpenav@users.noreply.github.com>
2023-11-01 09:52:18 -0600

    Innit Commit

---
+++ app/helpers/movies_helper.rb
@@ -0,0 +1,16 @@
+module MoviesHelper
+
+    def total_gross(movie)
+        if movie.flop?
+            "Flop!"
+        else
+            number_to_currency(movie.total_gross, precision: 0)
+        end
+    end
+
+    def day_and_time(movie)
+        movie.released_on.strftime("%B %d %Y") 
+    end
+
+
+end
